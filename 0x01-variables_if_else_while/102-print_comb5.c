fca8490669c3ae45ad3bf3feac1e4a150e497691
Paul Ibikunle <ibikunlepaulo@gmail.com>
2022-09-09 18:50:47 -0700

    lowercase

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if i != 98 || j != 99)
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
@@ -29,6 +29,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
  }
