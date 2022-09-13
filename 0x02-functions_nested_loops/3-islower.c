286e058dbd8530ba094e423f109e85280166c511
Paul Ibikunle <ibikunlepaulo@gmail.com>
2022-09-13 16:57:21 -0700

    lowercase alphabet

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks for lowercase character
- * @c - c single letter input
+ * @c - c is an integer value to test
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int c)
