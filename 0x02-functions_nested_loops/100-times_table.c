0b9ad17ad20bf495a73d8f2582e4b506a048fb68
Paul Ibikunle <ibikunlepaulo@gmail.com>
2022-09-14 12:56:03 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,67 @@
 #include "main.h"
+#include <stdbool.h>
 
 /**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
+ * _print_char - prints a character using _putchar
+ * @p: charater to print
+ * @s: size, number of times to print the character
+ * @is_put: boolean
+ */
+void _print_char(char p, int s, bool is_put)
+{
+	int i = 0;
+
+	if (is_put == true)
+		_putchar(',');
+
+	for (i = 0; i < s; i++)
+		_putchar(p);
+}
+
+/**
+ * print_times_table - prints the times table of nine (9)
+ * @n: int n size of table
+ * Return: void
  */
 void print_times_table(int n)
 {
-	return (0);
+	if (n >= 0 && n < 15)
+	{
+		int r, p, prd, var;
+
+		for (r = 0; r <= n; r++)
+		{
+			for (p = 0; p <= n; p++)
+			{
+				prd = r * p;
+				var = prd % 10;
+
+				if (p == 0)
+				{
+					_print_char('0', 1, false);
+				}
+				else if (prd < 10)
+				{
+					_print_char(' ', 3, true);
+					_print_char(var + '0', 1, false);
+				}
+				else if (prd >= 10 && prd < 100)
+				{
+					_print_char(' ', 2, true);
+					_print_char(((prd / 10) % 10) + '0', 1, false);
+					_print_char((prd % 10) + '0', 1, false);
+				}
+				else if (prd > 99 && prd < 1000)
+				{
+					_print_char(' ', 1, true);
+					_print_char(prd / 100 + '0', 1, false);
+					_print_char(((prd / 10) % 10) + '0', 1, false);
+					_print_char((prd % 10) + '0', 1, false);
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
+
+
