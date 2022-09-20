e5d43f29733b692bdc9aff87753f4b1fff946cce
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-09-20 02:39:50 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,41 +2,24 @@
 #include <stdio.h>
 #include <time.h>
 
-/**
- * main - random password generator for 101-crackme
- *
- * Return: always 0
- */
-int main(void)
+int main()
 {
-	int i, j, k, s;
-	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char p[58];
+    int counter = 0;
+    srand(time(NULL));
+    char randChar;
 
-	srand(time(NULL));
-	while (s != 2772)
-	{
-		i = k = s = 0;
-		while ((2772 - 122) > s)
-		{
-			j = rand() % 62;
-			p[i] = c[j];
-			s += c[j];
-			i++;
-		}
-		while (c[k])
-		{
-			if (c[k] == (2772 - s))
-			{
-				p[i] = c[k];
-				s += c[k];
-				i++;
-				break;
-			}
-			k++;
-		}
-	}
-	p[i] = '\0';
-	printf("%s", p);
-	return (0);
+    int  passwordLength;
+
+    printf("Type in a password Length \n");
+    scanf("%d", &passwordLength);
+
+    while(counter < passwordLength)
+    {
+        //seed random based on time
+        srand(time(NULL));
+        randChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[random () % 62];    
+        printf("%c", randChar);
+        counter++;
+    }
+    return 0;
 }
