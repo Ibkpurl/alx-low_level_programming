30f2773b4850048237adf3810b12a0969cd11e64
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-09-21 21:37:32 +0100

    reverse the content of an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,19 +9,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int *b, i, aux, k;
-	b = a;
+	int i = 0;
+	int aux;
 
-	for (i = 1; i < n; i++)
+	while (i < n--)
 	{
-		b++;
-	}
-
-	for (k = 0; k < i / 2; k++)
-	{
-		aux = a[k];
-		a[k] = *b;
-		*b = aux;
-		b--;
+		aux = a[i];
+		a[i++] = a[n];
+		a[n] = aux;
 	}
 }
