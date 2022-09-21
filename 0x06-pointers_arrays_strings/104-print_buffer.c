522898d48533dcd08d51ba4a2c578157018080a2
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-09-21 22:03:58 +0100

    function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,27 +1,61 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-  * string_toupper - function that change all lowercase letters to uppercase
-  *
-  * @p: String to return
-  *
-  * Return: String
-  */
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
 
-	char *string_toupper(char *p)
+// void print_line(char *c, int s, int l)
+// {
+// 	int j, k;
+
+// 	for (j = 0; j <= 9; j++)
+// 	{
+// 		if (j <= s)
+// 			printf("%02x", c[l * 10 + j]);
+// 		else
+// 			printf("  ");
+// 		if (j % 2)
+// 			putchar(' ');
+// 	}
+// 	for (k = 0; k <= s; k++)
+// 	{
+// 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+// 			putchar(c[l * 10 + k]);
+// 		else
+// 			putchar('.');
+// 	}
+// }
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-
-		int a;
-
-		for (a = 0; p[a] != '\0'; a++)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-
-			if (p[a] >= 97 && p[a] <= 122)
-			{
-				p[a] = p[a] - 32;
-			}
-
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
 	}
-
-	return (p);
+	if (size == 0)
+		putchar('\n');
 }
