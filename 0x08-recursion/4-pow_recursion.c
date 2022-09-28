57c73624c0fbec32e16330638cdf0ab6aadb7864
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-09-28 22:33:40 +0100

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,17 @@
-a
+#include "main.h"
+
+/**
+ * _pow_recursion - returns value of x raised to the power y
+ * @x: input one
+ * @y: input two
+ * Return: Always 0 (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
