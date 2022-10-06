7e870dc36a83abe9ec7cef68910899502b4a9ede
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-10-06 21:16:18 +0100

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a fuctiion that allocates memory using malloc
+ * @b: bytes 
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
