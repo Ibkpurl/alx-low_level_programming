a82fdb61e7721719d4f6b5a69d5aab746555b447
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-10-06 22:38:36 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,108 +3,81 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
- * Return: 1 if digit, 0 otherwise
+ * allocmem - function that allocates memory
+ * @nmemb: size of array
+ * @size: size of each element
+ * Return: pointer to new allocated memory
  */
-int _isdigit(int c)
-{
-  return (c >= '0' && c <= '9');
-}
+void *allocmem(unsigned int nmemb, unsigned int size);
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- * Return: integer length of string
+ * multiply - initialize array with 0 byte
+ * @s1: char 1
+ * @s2: char 2
+ * Return: nothing
  */
-int _strlen(char *s)
+void multiply(char *s1, char *s2)
 {
-	int i = 0;
+	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
+	char *ptr;
+	void *temp;
 
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
- * Return: the product big number string
- */
-char *big_multiply(char *s1, char *s2)
-{
-	char *r;
-	int l1, l2, a, b, c, x;
-
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
-
+	l1 = _length(s1);
+	l2 = _length(s2);
+	tmp = l2;
+	total_l = l1 + l2;
+	ptr = allocmem(sizeof(int), total_l);
+	temp = ptr;
 	for (l1--; l1 >= 0; l1--)
 	{
-		if (!_isdigit(s1[l1]))
+		f_digit = s1[l1] - '0';
+		res = 0;
+		l2 = tmp;
+
+		for (l2--; l2 >= 0; l2--)
 		{
-			free(r);
-			printf("Error\n"), exit(98);
+			s_digit = s2[l2] - '0';
+			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
+			ptr[l1 + l2 + 1] = res % 10;
+			res /= 10;
 		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
+		if (res)
+			ptr[l1 + l2 + 1] = res % 10;
 	}
-	return (r);
+
+	while (*ptr == 0)
+	{
+		ptr++;
+		total_l--;
+	}
+
+	for (i = 0; i < total_l; i++)
+		printf("%i", ptr[i]);
+
+	printf("\n");
+	free(temp);
 }
 
-
 /**
- * main - multiply two big number strings
- * @argc: the number of arguments
- * @argv: the argument vector
- * Return: Always 0 on success.
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: arguments array
+ * Return: 0 on success 98 on faliure
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *r;
-	int a, c, x;
+	char *n1 = argv[1];
+	char *n2 = argv[2];
 
-	if (argc != 3)
-		printf("Error\n"), exit(98);
+	if (argc != 3 || check_number(n1) || check_number(n2))
+		error_exit();
 
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
+	if (*n1 == '0' || *n2 == '0')
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
-	}
-	if (!a)
 		_putchar('0');
-	_putchar('\n');
-	free(r);
+		_putchar('\n');
+	}
+	else
+		multiply(n1, n2);
 	return (0);
 }
