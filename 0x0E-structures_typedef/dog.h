28e2509bbc7dec511520ce1fbf6dda8b32bf33fd
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-10-10 19:47:14 +0100

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,23 @@
-c
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - A new type representing dog.
+ * @name: name of a dog.
+ * @age: age of a dog.
+ * @owner: name of a dog owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
