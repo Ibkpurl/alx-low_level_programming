7797c29d127fbf406b7ee46c3a668e58e7a6f5c1
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2022-10-24 21:45:50 +0100

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that inserts node at given index
+ * insert_nodeint_at_index - A function that inserts node at given index
  * @head: A pointer to listint_t structure
  * @idx: The index of the list
  * @n: An integer data for new node
@@ -10,40 +10,40 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 1;
-	listint_t *new, *tmp;
+	listint_t *new_node, *temp;
 
 	if (head == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 
 	if (*head == NULL)
 	{
-		*head = new;
-		new->next = NULL;
-		new->n = n;
-		return (new);
+		*head = new_node;
+		new_node->next = NULL;
+		new_node->n = n;
+		return (new_node);
 	}
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		new->n = n;
-		*head = new;
-		return (new);
+		new_node->next = *head;
+		new_node->n = n;
+		*head = new_node;
+		return (new_node);
 	}
 
-	tmp = *head;
+	temp = *head;
 	while (i < idx)
 	{
-		tmp = tmp->next;
+		temp = temp->next;
 		i++;
 	}
 
-	new->n = n;
-	new->next = tmp->next;
-	tmp->next = new;
-	return (new);
+	new_node->n = n;
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
